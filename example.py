9891629457ae2acf464ee9d3f42818b0c7da1553
Sam Connolly <sdc1g08@soton.ac.uk>
2016-11-01 11:38:34 +0000

    Update example.py

--- example.py
+++ example.py
@@ -40,29 +40,29 @@ def Fix_BL(v,A,v_bend,a_high,c):
 datalc.Fit_PSD(initial_params=[1,0.001,2.5,0],model=Fix_BL)
 
 # create mixture distribution to fit to PDF
-#mix_model = Mixture_Dist([st.gamma,st.lognorm],[3,3],[[[2],[0]],[[2],[0],]])
-#
-#
-## estimate underlying variance of data light curve
-#datalc.STD_Estimate()
-#
-## simulate artificial light curve with Timmer & Koenig method
-#tklc = Simulate_TK_Lightcurve(datalc,BendingPL, (A,v_bend,a_low,a_high,c),
-#                                RedNoiseL,aliasTbin,RandomSeed)
-#
-## simulate artificial light curve with Emmanoulopoulos method, scipy distribution
-#delc_mod = Simulate_DE_Lightcurve(datalc,BendingPL, (A,v_bend,a_low,a_high,c),
-#                               mix_model, (kappa, theta, lnsig, np.exp(lnmu),
-#                                                              weight,1-weight))
-#
-## simulate artificial light curve with Emmanoulopoulos method, using the PSD
-## and PDF of the data light curve, with default parameters (bending power law
-## for PSD and mixture distribution of gamma and lognormal distribution for PDF)
-#delc = datalc.Simulate_DE_Lightcurve()
-#  
-## save the simulated light curve as a txt file
-#delc.Save_Lightcurve('lightcurve.dat')
-#                                  
-## plot lightcurves and their PSDs ands PDFs for comparison
-#Comparison_Plots([datalc,tklc,delc,delc_mod],names=["Data LC","Timmer \& Koenig",
-#             "Emmanoulopoulos from model","Emmanoulopoulos from data"],bins=25)
+mix_model = Mixture_Dist([st.gamma,st.lognorm],[3,3],[[[2],[0]],[[2],[0],]])
+
+
+# estimate underlying variance of data light curve
+datalc.STD_Estimate()
+
+# simulate artificial light curve with Timmer & Koenig method
+tklc = Simulate_TK_Lightcurve(datalc,BendingPL, (A,v_bend,a_low,a_high,c),
+                                RedNoiseL,aliasTbin,RandomSeed)
+
+# simulate artificial light curve with Emmanoulopoulos method, scipy distribution
+delc_mod = Simulate_DE_Lightcurve(datalc,BendingPL, (A,v_bend,a_low,a_high,c),
+                               mix_model, (kappa, theta, lnsig, np.exp(lnmu),
+                                                              weight,1-weight))
+
+# simulate artificial light curve with Emmanoulopoulos method, using the PSD
+# and PDF of the data light curve, with default parameters (bending power law
+# for PSD and mixture distribution of gamma and lognormal distribution for PDF)
+delc = datalc.Simulate_DE_Lightcurve()
+  
+# save the simulated light curve as a txt file
+delc.Save_Lightcurve('lightcurve.dat')
+                                  
+# plot lightcurves and their PSDs ands PDFs for comparison
+Comparison_Plots([datalc,tklc,delc,delc_mod],names=["Data LC","Timmer \& Koenig",
+             "Emmanoulopoulos from model","Emmanoulopoulos from data"],bins=25)
